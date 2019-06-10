87caae2fb3338ccbbd201ca0585df460bbfe4f68
Slađan Ristić <sladiri@users.noreply.github.com>
2019-06-10 14:56:00 +0200

    write: rename and reorder fields

--- src/bolt-on-shim/write.js
+++ src/bolt-on-shim/write.js
@@ -6,7 +6,7 @@ export const isValidWrite = (item, keyMayBeNull = false) => {
   return (
     item !== null &&
     typeof item === "object" &&
-    typeof item._v === "string" &&
+    typeof item.version === "string" &&
     ((keyMayBeNull && item.key === null) || typeof item.key === "string") &&
     areValidDeps(item.deps) && // eslint-disable-line no-use-before-define
     isValidClock(item.clock)
@@ -14,6 +14,16 @@ export const isValidWrite = (item, keyMayBeNull = false) => {
 };
 
 export const createWrite = (nodeId, key = null, value, deps) => {
-  const clock = createClock(nodeId, deps);
-  return { _v: version, nodeId, key, value, deps, clock };
+  const clock = createClock(
+    nodeId,
+    0 /* TODO: remove, 0 is just easier to read while developing */,
+  );
+  return {
+    key,
+    value,
+    version,
+    nodeId,
+    clock,
+    deps,
+  };
 };
