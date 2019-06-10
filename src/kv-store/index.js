7a39046eb1deebacf6926ff4a8a39aef6956fb48
Slađan Ristić <sladiri@users.noreply.github.com>
2019-06-10 14:55:43 +0200

    kv-store: fix instance immutability

--- src/kv-store/index.js
+++ src/kv-store/index.js
@@ -1,7 +1,7 @@
 import { get, set } from "./store.js";
 
 export default (storage = new Map()) => {
-  return Object.seal(
+  return Object.freeze(
     Object.assign(Object.create(null), {
       get: (key) => get(storage, key),
       set: (key, value) => set(storage, key, value),
