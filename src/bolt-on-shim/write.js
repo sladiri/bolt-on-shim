586abf34fde7d40a76601bd11c1a1aaf322a48ae
Slađan Ristić <sladiri@users.noreply.github.com>
2019-06-12 18:33:22 +0200

    write/createWrite: key must not be null

--- src/bolt-on-shim/write.js
+++ src/bolt-on-shim/write.js
@@ -2,18 +2,20 @@ import { areValidDeps, createClock, isValidClock } from "./clock.js";
 
 const version = "1";
 
-export const isValidWrite = (item, keyMayBeNull = false) => {
+export const isValidWrite = (item) => {
   return (
     item !== null &&
     typeof item === "object" &&
     typeof item.version === "string" &&
-    ((keyMayBeNull && item.key === null) || typeof item.key === "string") &&
+    item.key.version > 0 &&
+    typeof item.key === "string" &&
+    item.key.length > 0 &&
     areValidDeps(item.deps) && // eslint-disable-line no-use-before-define
     isValidClock(item.clock)
   );
 };
 
-export const createWrite = (nodeId, key = null, value, deps, tick) => {
+export const createWrite = (nodeId, key, value, deps, tick) => {
   const clock = createClock(nodeId, tick);
   return {
     key,
